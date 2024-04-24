268696dd7e91bf7983aa80fd1a0d6eb7c81d5a85
tysmith816 <tysmith816@gmail.com>
2024-04-23 20:02:33 -0400

    Formatted the data in a table with some placeholder css

--- webserver/app.js
+++ webserver/app.js
@@ -3,16 +3,27 @@ const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
+    if (req.url === "/") {
+        fs.readFile('index.html', function(error, data) {
+            if (error) {
+                res.writeHead(404)
+                res.write('Error: File Not Found')
+            } else {
+                res.setHeader('Content-type', 'text/html');
+                res.write(data)
+                res.statusCode = 200;
+                res.end();
+            }
+        })
+    }
+    else if (req.url == '/styles.css') {
+        res.setHeader('Content-type', 'text/css');
+        res.write(fs.readFileSync('styles.css'));
+        res.end();
+    } else {
+        res.write("invalid request")
+        res.end();
+    }
 })
 
 server.listen(port, function(error) {
